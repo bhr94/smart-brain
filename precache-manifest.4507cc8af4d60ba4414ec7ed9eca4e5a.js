2682a6bedda26dd30b684b8fba033b2322b0c21c
Bahar Mutadayin <baharmutadayin@Bahars-MacBook-Pro.local>
2020-10-03 10:43:00 -0700

    Updates

---
+++ precache-manifest.4507cc8af4d60ba4414ec7ed9eca4e5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abccd064e0995cd22a394cde236a8a2d",
+    "url": "/smart-brain-client/index.html"
+  },
+  {
+    "revision": "1162e6784d1b9fa5434e",
+    "url": "/smart-brain-client/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b3de02f872acdd89e540",
+    "url": "/smart-brain-client/static/css/main.792be76a.chunk.css"
+  },
+  {
+    "revision": "1162e6784d1b9fa5434e",
+    "url": "/smart-brain-client/static/js/2.2ae18d4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain-client/static/js/2.2ae18d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3de02f872acdd89e540",
+    "url": "/smart-brain-client/static/js/main.f40c713d.chunk.js"
+  },
+  {
+    "revision": "31930fd96666b47fd930",
+    "url": "/smart-brain-client/static/js/runtime-main.1b14ed15.js"
+  }
+]);
